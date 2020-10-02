12e5638cde9b5757469a1fc313b2e64d361f1207
GAL1L31O <44536443+GAL1L31O@users.noreply.github.com>
2020-10-02 13:10:31 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,6 @@ echo "Installing Python..."
 pkg install python
 echo "Installing Scapy from Python..."
 pip install scapy
+echo "Installing argparse from Python..."
+pip install argparse
 chmod +x *
